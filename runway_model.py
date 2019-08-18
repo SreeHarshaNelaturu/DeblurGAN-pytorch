e7470e6e235f7fa0964123436df0b4d3d9940892
SreeHarshaNelaturu <nelaturu.harsha@gmail.com>
2019-08-19 04:37:24 +0530

    final commit hopefully

--- runway_model.py
+++ runway_model.py
@@ -1,17 +1,13 @@
-import numpy as np
-import torch
 import runway
-from runway.data_types import *
 from deblur_image import *
-@runway.setup(options={'checkpoint': file(extension='.pth')})
-def setup(opts):
-    print("++++++ Loading Model +++++++")
-    model = torch.load(opts['checkpoint'])
-    print("++++++ Model Loaded ++++++")
 
+@runway.setup(options={'checkpoint': runway.file(extension='.pth')})
+def setup(opts):
+    model = torch.load(opts['checkpoint'])
     return model
-inputs = {"blurred": image}
-outputs = {"deblurred": image}
+
+inputs = {"blurred": runway.image}
+outputs = {"deblurred": runway.image}
 
 
 @runway.command('deblur_image', inputs=inputs, outputs=outputs, description='Deblur an Image')
@@ -32,22 +28,22 @@ def deblur_image(model, inputs):
 
     # start to deblur
     with torch.no_grad():
-        blurred = inputs['blurred']
-        transform = transforms.Compose([transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-        blurred = transform(blurred)
+        blurred = inputs["blurred"]
         h = blurred.size[1]
         w = blurred.size[0]
         new_h = h - h % 4 + 4 if h % 4 != 0 else h
         new_w = w - w % 4 + 4 if w % 4 != 0 else w
         blurred = transforms.Resize([new_h, new_w], Image.BICUBIC)(blurred)
+        transform = transforms.Compose([
+            transforms.ToTensor(),  # convert to tensor
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        blurred = transform(blurred)
         blurred.unsqueeze_(0)
         print(blurred.shape)
         blurred = blurred.to(device)
         deblurred = generator(blurred)
         deblurred_img = to_pil_image(denormalize(deblurred).squeeze().cpu())
 
-        #deblurred_img.save("./deblurred.png")
         return deblurred_img
 if __name__ == '__main__':
     runway.run()
